f04dae391ebb6bec20829b6d205387aef989f62a
naxifa <maliha1@usf.edu>
2024-08-31 21:30:37 -0400

    Task 1 - Employee Information

---
+++ cc_example.js
@@ -0,0 +1,11 @@
+// Task 1 : Variables and Data Types
+
+let employeeName = "Nara Smith"
+const employeeID = 10023;
+var isActive = true;
+
+console.log(employeeName, typeof(employeeName));
+console.log(employeeID, typeof(employeeID));
+console.log(isActive, typeof(isActive));
+
+
