f6c877f6da7ac0fb5048a56ce62778ba8b8cf393
naxifa <maliha1@usf.edu>
2024-08-31 23:28:59 -0400

    Task 3 - Financial Calculations

--- cc_1.js
+++ cc_1.js
@@ -18,7 +18,7 @@ const productDetails = {
         name: products,
         price: 675.00,
         inStock: 12345.00, 
-};
+}
 
 console.log(products, productDetails);
 
@@ -26,3 +26,19 @@ console.log(products, productDetails);
 
 // Task 3 : Assignment Operators
 
+let accountBalance = 123456.00;
+
+accountBalance += 2345.00;
+console.log(accountBalance);
+
+accountBalance -= 3456.00;
+console.log(accountBalance);
+
+accountBalance *= 5.00;
+console.log(accountBalance);
+
+accountBalance /= 7.50;
+console.log(accountBalance);
+
+accountBalance %= 2.50;
+console.log(accountBalance);
